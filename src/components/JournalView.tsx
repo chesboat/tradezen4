8d1e27b6087f0d4fff1250507532a59aa1531ef1
chesboat <chesbo@gmail.com>
2025-08-29 00:27:00 -0400

    Revert "Journal: force remote reflection load on view mount; revert effectiveAccountId to null when none (prevents bad 'all' id)"
    This reverts commit 858a102cb81e6899e7afa2b9a23d0369a8e5756e.

--- src/components/JournalView.tsx
+++ src/components/JournalView.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import { useReflectionTemplateStore } from '@/store/useReflectionTemplateStore';
 import { JournalTimeline } from './JournalTimeline';
 import { SmartTagFilterBar } from './SmartTagFilterBar';
 
 export const JournalView: React.FC = () => {
-  // Ensure reflections are loaded on mount to avoid blank mobile page
-  React.useEffect(() => {
-    try { (useReflectionTemplateStore.getState()._loadRemote as any)?.(); } catch {}
-  }, []);
   return (
     <div className="h-full flex flex-col">
       <SmartTagFilterBar />
