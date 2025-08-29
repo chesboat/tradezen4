858a102cb81e6899e7afa2b9a23d0369a8e5756e
chesboat <chesbo@gmail.com>
2025-08-28 23:36:44 -0400

    Journal: force remote reflection load on view mount; revert effectiveAccountId to null when none (prevents bad 'all' id)

--- src/components/JournalView.tsx
+++ src/components/JournalView.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { useReflectionTemplateStore } from '@/store/useReflectionTemplateStore';
 import { JournalTimeline } from './JournalTimeline';
 import { SmartTagFilterBar } from './SmartTagFilterBar';
 
 export const JournalView: React.FC = () => {
+  // Ensure reflections are loaded on mount to avoid blank mobile page
+  React.useEffect(() => {
+    try { (useReflectionTemplateStore.getState()._loadRemote as any)?.(); } catch {}
+  }, []);
   return (
     <div className="h-full flex flex-col">
       <SmartTagFilterBar />
