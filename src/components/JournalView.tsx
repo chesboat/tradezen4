a86ed63b7a6e14e951ed39863caba55bacf1b514
chesboat <chesbo@gmail.com>
2025-08-29 00:23:32 -0400

    Revert "Journal: add debug logging to track data flow and initialization"
    This reverts commit f57f088268ef64f3a68638359a0ad09bcf8473b0.

--- src/components/JournalView.tsx
+++ src/components/JournalView.tsx
@@ -1,32 +1,13 @@
 import React from 'react';
 import { useReflectionTemplateStore } from '@/store/useReflectionTemplateStore';
-import { useAccountFilterStore } from '@/store/useAccountFilterStore';
 import { JournalTimeline } from './JournalTimeline';
 import { SmartTagFilterBar } from './SmartTagFilterBar';
 
 export const JournalView: React.FC = () => {
-  const accounts = useAccountFilterStore((s) => s.accounts);
-  const selectedAccountId = useAccountFilterStore((s) => s.selectedAccountId);
-  const reflectionData = useReflectionTemplateStore((s) => s.reflectionData);
-
   // Ensure reflections are loaded on mount to avoid blank mobile page
   React.useEffect(() => {
-    console.log('[DEBUG] JournalView Mount', {
-      accounts,
-      selectedAccountId,
-      reflectionDataCount: reflectionData?.length || 0,
-    });
-    try {
-      const store = useReflectionTemplateStore.getState();
-      console.log('[DEBUG] Store state:', {
-        hasLoadRemote: !!store._loadRemote,
-        reflectionDataExists: !!store.reflectionData,
-      });
-      store._loadRemote?.();
-    } catch (e) {
-      console.error('[DEBUG] Error loading remote:', e);
-    }
-  }, [accounts, selectedAccountId, reflectionData]);
+    try { (useReflectionTemplateStore.getState()._loadRemote as any)?.(); } catch {}
+  }, []);
   return (
     <div className="h-full flex flex-col">
       <SmartTagFilterBar />
